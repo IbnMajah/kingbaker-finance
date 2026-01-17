2808a4c10a17565d30ae29738942f840644445a9
phantom-merger <149815696+phantom-merger@users.noreply.github.com>
2026-01-17 13:58:38 +0000

    refactor: removed mark as paid on invoices

--- resources/js/Pages/Invoices/Show.vue
+++ resources/js/Pages/Invoices/Show.vue
@@ -233,7 +233,7 @@
           Record Payment
         </button>
 
-        <button
+        <!-- <button
           v-if="invoice.status !== 'paid'"
           @click="markAsPaid"
           :disabled="processing"
@@ -242,7 +242,7 @@
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path>
           </svg>
           {{ processing ? 'Processing...' : 'Mark as Paid' }}
-        </button>
+        </button> -->
 
         <button
           @click="confirmDelete"
